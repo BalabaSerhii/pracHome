3ad193f9c71c2d7f1af7c2d97583422aadce5574
BalabaSerhii <balabaserhii@gmail.com>
2023-09-01 21:12:31 +0200

    x

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
 
+const test = galleryItems
+  .map((imgEl) => {
+    return `<li class="gallery__item">
+	<a class="gallery__link" target="_blank" ">
+	  <img
+		 class="gallery__image"
+		 src="${imgEl.preview}"
+		 data-source="${imgEl.original}"
+		 alt="${imgEl.description}"
+	  />
+	</a>
+ </li>`;
+  })
+  .join("");
 
+gallery.insertAdjacentHTML("afterbegin", test);
 
-const gallery = document.querySelector('.gallery');
+// const ttt = evt.target.classList.contains('gallery__image');
 
+// {
 
+// 	gallery.create(`
+// 		<img width="1400" height="900" src="https://placehold.it/1400x900">
+// 	`).show()
 
-const addImg = document.createElementNS('')
+// }
+
+// gallery.addEventListener('click', openImg);
+
+// function openImg(evt) {
+// 	// if (!evt.target.classList.contains('gallery__image')) {
+// 	// 	return
+// 	// } else {
+// 	// 	basicLightbox.create(`
+// 	// 	<img width="1400" height="900" src="https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg">
+// 	// `).show()
+// 	// }
+
+// 	gallery.create(`
+// 		<img width="1400" height="900" src="https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg">
+// 	`).show()
+// 	console.log(evt.target)
+	
+// }
