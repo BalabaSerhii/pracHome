63d346971acc3dac16e379fe254f8d32e2a324fe
BalabaSerhii <balabaserhii@gmail.com>
2023-09-02 21:38:51 +0200

    norm

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,18 @@
 import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
 
 const test = galleryItems
-  .map((imgEl) => {
+  .map(({ preview, original, description }) => {
     return `<li class="gallery__item">
-	<a class="gallery__link" target="_blank" ">
+	<a class="gallery__link"  href="large-image.jpg">
 	  <img
 		 class="gallery__image"
-		 src="${imgEl.preview}"
-		 data-source="${imgEl.original}"
-		 alt="${imgEl.description}"
+		 src="${preview}"
+		 data-source="${original}"
+		 alt="${description}"
 	  />
 	</a>
  </li>`;
@@ -20,30 +21,31 @@ const test = galleryItems
 
 gallery.insertAdjacentHTML("afterbegin", test);
 
-// const ttt = evt.target.classList.contains('gallery__image');
+gallery.addEventListener("click", fu);
 
-// {
+function fu(evt) {
+  evt.preventDefault();
+  const source = evt.target.dataset.source;
+  if (evt.target.tagName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `
+		<img width="1400" height="900" src="${source}">
+	`,
+    {
+      onClose: () => {
+        window.removeEventListener("keydown", finish);
+      },
+    }
+  );
+  instance.show();
 
-// 	gallery.create(`
-// 		<img width="1400" height="900" src="https://placehold.it/1400x900">
-// 	`).show()
+  const finish = (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  };
 
-// }
-
-// gallery.addEventListener('click', openImg);
-
-// function openImg(evt) {
-// 	// if (!evt.target.classList.contains('gallery__image')) {
-// 	// 	return
-// 	// } else {
-// 	// 	basicLightbox.create(`
-// 	// 	<img width="1400" height="900" src="https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg">
-// 	// `).show()
-// 	// }
-
-// 	gallery.create(`
-// 		<img width="1400" height="900" src="https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg">
-// 	`).show()
-// 	console.log(evt.target)
-	
-// }
+  window.addEventListener("keydown", finish);
+}
